ea5bd50189e6f581b86dda7b0d7ccabe964ecc63
vsellemi <59780031+vsellemi@users.noreply.github.com>
2020-03-19 19:34:41 -0700

    Update PriceRisk_OLS.R

--- PriceRisk_OLS.R
+++ PriceRisk_OLS.R
@@ -1,11 +1,9 @@
-rm(list=ls())
-
 library("glmnet")
 library("rapportools")
 library("pracma")
 library("matlib")
 
-PriceRisk_OLS3 <- function(Ri,gt,ht) {
+PriceRisk_OLS <- function(Ri,gt,ht) {
   # for the factors gt by controling ht
   # using the Feng, Giglio and Xiu (2017) approach.
   
@@ -36,26 +34,27 @@ PriceRisk_OLS3 <- function(Ri,gt,ht) {
   
   cov_h <- matrix(NaN, nrow = n, ncol = p)
   for (nn in 1:n){
-    temp <- cov( cbind(t(Ri[nn,]), t(ht)) )
-    cov_h[nn,] <- temp[1,2:ncol(cov_h)]
+    temp <- cov( cbind((as.matrix(Ri[nn,])), t(ht)) )
+    cov_h[nn,] <- temp[1,2:ncol(temp)]
   }
   cov_g <- matrix(NaN, nrow = n, ncol = d)
   for (nn in 1:n) {
-    temp <- cov( cbind(t(Ri[nn,]),t(gt)) )
-    cov_g[nn,] <- temp[1,2:ncol(cov_g)]
+    temp <- cov( cbind(as.matrix(Ri[nn,]), t(as.matrix(gt))) )
+    cov_g[nn,] <- temp[1,2:ncol(temp)]
   }
   
   ER <- rowMeans(Ri)
   
   # for later use
-  M0 <- eye(n) - t(rep(1,n)) %*% inv(rep(1,n) %*% t(rep(1,n))) %*% rep(1,n)
+  ones <- as.matrix(rep(1,n))
+  M0   <- eye(n) - ones%*% inv(t(ones)%*%ones)%*%t(ones)
   
   # for no selection OLS
   # no selection estimate
   X <- cbind(cov_g,cov_h)
-  X_zero <- cbind(t(rep(1,n)),cov_g,cov_h)
+  X_zero <- cbind(rep(1,n),cov_g,cov_h)
   lambda_full_zero <- inv( t(X_zero) %*% X_zero )%*%(t(X_zero)%*%ER)
-  lambda_full      <- inv(t(X)%*%M0*X)%*%(t(X)%*%M0%*%ER)
+  lambda_full      <- inv(t(X)%*%M0%*%X)%*%(t(X)%*%M0%*%ER)
   lambdag_ols      <- lambda_full[1:d]
   rm(X)
   nomissing <- which(colSums(is.nan(rbind(ht,gt))) == 0)
@@ -65,7 +64,7 @@ PriceRisk_OLS3 <- function(Ri,gt,ht) {
   zthat3 <- matrix(NaN,nrow = d, ncol = Lnm)
   for (i in 1:d) {
     M_mdl <- eye(Lnm) - t(ht[,nomissing])%*%inv(ht[,nomissing]%*%t(ht[,nomissing]))%*%ht[,nomissing]
-    zthat3[i,] <- M_mdl%*%t(gt[i,nomissing])
+    zthat3[i,] <- M_mdl%*%as.matrix(gt[i,nomissing])
     rm(M_mdl)
   }
   Sigmazhat3 <- zthat3%*%t(zthat3)/Lnm
@@ -75,15 +74,15 @@ PriceRisk_OLS3 <- function(Ri,gt,ht) {
   i <- 0
   for (l in nomissing) {
     i <- i+1
-    mt <- 1 - t(lambda_full)%*%rbind(gt[,l],ht[,l])
-    temp3 <- temp3 + mt^2* (inv(Sigmazhat3)%*%zthat[,i]%*%t(zthat3[,i])%*%inv(Sigmazhat3) )
+    mt <- 1 - t(lambda_full)%*%rbind(as.matrix(gt[,l]),as.matrix(ht[,l]))
+    temp3 <- temp3 + mt^2* (inv(Sigmazhat3)%*%zthat3[,i]%*%t(zthat3[,i])%*%inv(Sigmazhat3) )
   }
   avar_lambdag3 <- diag(temp3) / Lnm
   se3 <- sqrt(avar_lambdag3/Lnm)
   rm(temp3)
   
   # scaled lambda
-  V_bar <- vt - rowMeans(vt)%*%rep(1,Lnm)
+  V_bar <- vt - as.matrix(rowMeans(vt))%*%t(as.matrix(rep(1,Lnm)))
   var_v <- V_bar%*%t(V_bar) / Lnm
   
   lambda_ols <- diag(var_v)%*%lambda_full
@@ -109,7 +108,7 @@ PriceRisk_OLS3 <- function(Ri,gt,ht) {
   
   for (t in 1:Lnm){
     Bt <- inv(sigmavhat)%*%vtbar[,t]%*%t(vtbar[,t])%*%inv(sigmavhat)%*%Gammahat
-    At <- inv(sigmavhat)%*%vtbat[,t]%*%t(vtbar[,t])%*%inv(sigmavhat)%*%(t(Gamma_hat)%*%inv(sigmavhat)%*%vtbar[,t])
+    At <- inv(sigmavhat)%*%vtbar[,t]%*%t(vtbar[,t])%*%inv(sigmavhat)*drop(t(Gammahat)%*%inv(sigmavhat)%*%vtbar[,t])
     avar   <- avar - 2*At/Lnm + Bt%*%t(Bt) / Lnm
     meanBt <- meanBt + Bt/Lnm
   }
